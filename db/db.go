64ae356ed57802c0ef23245243ef7792b27c4734
ko44d <koyoshid622@gmail.com>
2024-05-25 00:25:52 +0900

    ここまで書いた内容を追加

---
+++ db/db.go
@@ -0,0 +1,28 @@
+package db
+
+import (
+	"fmt"
+	"github.com/joho/godotenv"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+	"log"
+	"os"
+)
+
+func NewDB() *gorm.DB {
+	if os.Getenv("ENV") == "dev" {
+		err := godotenv.Load()
+		if err != nil {
+			log.Fatalln(err)
+		}
+	}
+	url := fmt.Sprintf("postgres://%s:%s@%s:%s/%s", os.Getenv("POSTGRES_USER"),
+		os.Getenv("POSTGRES_PW"), os.Getenv("POSTGRES_HOST"),
+		os.Getenv("POSTGRES_PORT"), os.Getenv("POSTGRES_DB"))
+	db, err := gorm.Open(postgres.Open(url), &gorm.Config{})
+	if err != nil {
+		log.Fatalln(err)
+	}
+	fmt.Println("Connected to PostgreSQL")
+	return db
+}
